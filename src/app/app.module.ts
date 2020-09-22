bdcd654fdc3cd827e15ce1eada8e5456f5297a8a
Sherlin Toriz Pérez <sherlintp09@gmail.com>
2020-09-22 17:10:46 -0500

    tur tas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+// Rutas
+import { APP_ROUTING } from './app.routes';
+
+
+// Servicios
+
+
+// componentes
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
@@ -18,7 +25,7 @@ import { HeroesComponent } from './components/heroes/heroes.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    APP_ROUTING
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/app.routes.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+
+
+
+const APP_ROUTES: Routes = [
+    { path: 'home', component: HomeComponent },
+    { path: '**', pathMatch: 'full', redirectTo: 'home'}
+];
+
+export const APP_ROUTING = RouterModule.forRoot(APP_ROUTES);
