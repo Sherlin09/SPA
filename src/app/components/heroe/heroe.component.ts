f9c5dc5652de8cc6bfecd68ace2a2865886bd40c
Sherlin Toriz Pérez <sherlintp09@gmail.com>
2020-09-23 15:02:12 -0500

    Recibiendo parametros por URL

--- src/app/components/heroe/heroe.component.ts
+++ src/app/components/heroe/heroe.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { HeroesService } from '../../servicios/heroes.service';
 
 @Component({
   selector: 'app-heroe',
@@ -7,11 +8,16 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class HeroeComponent  {
 
-  constructor( private activateRoute: ActivatedRoute ) {
+  heroe:any = {};
+
+  constructor( private activateRoute: ActivatedRoute,
+               private _heroesService: HeroesService
+ ) {
 
     this.activateRoute.params.subscribe( params => {
-      console.log( params['id'] );
-    } )
+    this.heroe = this._heroesService.getHeroe( params['id'] );
+    
+    });
    }
 
 
