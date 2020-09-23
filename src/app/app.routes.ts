77934d89667d586cc787ff8ccd5812368ec9aa51
Sherlin Toriz Pérez <sherlintp09@gmail.com>
2020-09-23 16:53:53 -0500

    tarea2

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { HomeComponent } from './components/home/home.component';
 import { HeroesComponent } from './components/heroes/heroes.component';
 import { AboutComponent } from './components/about/about.component';
 import { HeroeComponent } from './components/heroe/heroe.component';
+import { BuscadorComponent } from './components/buscador/buscador.component';
 
 
 const APP_ROUTES: Routes = [
@@ -11,6 +12,7 @@ const APP_ROUTES: Routes = [
     { path: 'heroes', component: HeroesComponent },
     { path: 'about', component: AboutComponent },
     { path: 'heroe/:id', component: HeroeComponent },
+    { path: 'buscar/:termino', component: BuscadorComponent },
     { path: '**', pathMatch: 'full', redirectTo: 'home'}
 ];
 
