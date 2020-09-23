6b4336eaa898769861e7dc1d384d24ded38919a2
Sherlin Toriz Pérez <sherlintp09@gmail.com>
2020-09-23 16:07:10 -0500

    buscador

--- src/app/servicios/heroes.service.ts
+++ src/app/servicios/heroes.service.ts
@@ -68,6 +68,24 @@ export class HeroesService {
     getHeroe( idx: string ){
       return this.heroes[idx];
     }
+
+    buscarHeroes( termino:string ):Heroe[]{
+
+      let heroesArr:Heroe[] = [];
+      termino = termino.toLowerCase();
+
+      for( let heroe of this.heroes ){
+
+        let nombre = heroe.nombre.toLowerCase();
+
+        if ( nombre.indexOf( termino ) >= 0 ){
+          heroesArr.push( heroe )
+        }
+        
+      }
+
+      return heroesArr;
+    }
 }
 
 
